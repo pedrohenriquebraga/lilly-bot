2442e4aeaf412039d2e631cc6ad47bdc8c9cd3d2
pedrohenriquebraga <gamesantos7@gmail.com>
2020-09-13 12:29:04 -0300

    remove the folder 'utils'

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const Discord = require('discord.js')
 const client = new Discord.Client()
 const config = require('./config.json')
-const utils = require('./lilly/utils')
 
 
 client.on('message', msg => {
