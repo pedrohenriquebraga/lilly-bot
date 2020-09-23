460c4b71527e1d8096153aabeb2254d6ac7d56ae
pedrohenriquebraga <gamesantos7@gmail.com>
2020-09-23 12:19:41 -0300

    update command "$user"

--- src/commands/userinfo.js
+++ src/commands/userinfo.js
@@ -7,8 +7,31 @@ module.exports = {
     guildOnly: false,
     usage: '``$userinfo <?usuário>``',
     aliases: ['infousuario', 'usuario', 'user'],
-    execute(msg, args) {
+    async execute(msg, args) {
         const user = msg.mentions.users.first() || msg.author
+
+        console.log()
+        const date = new Date()
+        const actuallyYear = parseInt(date.getFullYear())
+
+        const userCreatedAt = user.createdAt.toString()
+    
+        let userJoinedAt = msg.member.joinedAt
+
+        if (args[0]) {
+            userJoinedAt = msg.mentions.members.first().joinedAt
+        }
+
+        const userCreatedDates = userCreatedAt.split(' ')
+        const userJoinedDates = userJoinedAt.toString().split(' ')
+
+        console.log(userCreatedDates)
+        console.log(userJoinedDates)
+
+        const userCreatedAccount = actuallyYear - parseInt(userCreatedDates[3])
+        const userJoinedServer = actuallyYear - parseInt(userJoinedDates[3])
+        
+
         const serverInfoEmbed = new Discord.MessageEmbed()
             .setColor('#ff0092')
             .setTitle(`📝 Informações do Usuário`)
@@ -17,7 +40,9 @@ module.exports = {
             .addFields(
                 {name: '▶ Nome de Usuário', value: `${user.username}`},
                 {name: '🆔 ID do Usuário', value: `${user.id}`},
-                {name: '🏷️ Tag do Usuário', value: `${user.discriminator}`}
+                {name: '🏷️ Tag do Usuário', value: `${user.discriminator}`},
+                {name: '📅 Criado há', value: `${userCreatedAccount} ano(s) atrás às ${userCreatedDates[4]}`},
+                {name: '📨 Entrou há', value: `${userJoinedServer} ano(s) atrás às ${userJoinedDates[4]}`},
             )
         msg.reply('', serverInfoEmbed)
     }
