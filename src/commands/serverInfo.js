f947a2c0500c77f5b762e0ed789ae4834b77da60
pedrohenriquebraga <gamesantos7@gmail.com>
2020-09-23 10:06:38 -0300

    Now, the command "$server" return the createdAt

--- src/commands/serverInfo.js
+++ src/commands/serverInfo.js
@@ -7,6 +7,14 @@ module.exports = {
     args: false,
     aliases: ['infoservidor', 'servidor', 'server'],
     execute(msg, args) {
+        const date = new Date()
+        const  actuallyYear = date.getFullYear()
+
+        const createdAt = msg.guild.createdAt.toString()
+        const dateCreate = createdAt.split(' ')
+
+        const yearCreate = parseInt(actuallyYear) - parseInt(dateCreate[3])
+    
         const serverInfoEmbed = new Discord.MessageEmbed()
             .setColor('#ff0092')
             .setTitle('📝 Informações do Server')
@@ -16,8 +24,10 @@ module.exports = {
                 {name: '🏷️ Nome do Servidor', value: `${msg.guild.name}`},
                 {name: '👥 Total de Membros', value: `${msg.guild.memberCount} membros`},
                 {name: '👑 Dono do Servidor', value: '``' + msg.guild.owner.user.username + '``'},
+                {name: '📅 Criado há', value: `${yearCreate} ano(s) atrás às ${dateCreate[4]}`},
                 {name: '🌎 Região', value: `${msg.guild.region.toUpperCase()}`}
             )
+    
         msg.reply('', serverInfoEmbed)
     }
 }
