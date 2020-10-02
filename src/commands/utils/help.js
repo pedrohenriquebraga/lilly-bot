ad616951a1a4376fb46e0d2586d719ad892929de
pedrohenriquebraga <gamesantos7@gmail.com>
2020-10-02 11:22:19 -0300

    Now, the command "$help" show link to vote

--- src/commands/utils/help.js
+++ src/commands/utils/help.js
@@ -16,6 +16,7 @@ module.exports = {
             .addFields(
                 { name: '📨 Me convide para seu server', value: `[Ficarei muito feliz em fazer parte de seu servidor!](https://discord.com/api/oauth2/authorize?client_id=754548334328283137&permissions=8&scope=bot)` },
                 { name: '👷‍♂️ Entre em contato com o suporte', value: `[Minha equipe de suporte vai ficar feliz em receber suas dúvidas em meu servidor!](https://discord.gg/SceHNfZ)` },
+                { name: '🗳 Vote na Lilly', value: `[Vote em mim para que eu consiga mais amiguinhos!!](https://zuraaa.com/bots/754548334328283137)` },
                 { name: '🌐 Acesse meu Website', value: '*(em breve)*' },
                 { name: '<> Meus comandos', value: `*(em breve)*` }
             )
