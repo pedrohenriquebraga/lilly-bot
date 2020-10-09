22b8187ea65e747547f029a60622596d5f5918cd
pedrohenriquebraga <gamesantos7@gmail.com>
2020-10-09 17:51:18 -0300

    disable/enanable commands of economy

---
+++ src/commands/configs/economy.js
@@ -0,0 +1,31 @@
+const guilds = require('../../controllers/guildsController')
+
+module.exports = {
+    name: 'economy',
+    description: 'Habilita ou desabilita os comandos de economia do servidor',
+    args: false,
+    guildOnly: true,
+    economy: false,
+    premium: false,
+    userPermissions: 'Gerenciar o servidor ou Administrador',
+    lillyPermissions: 'Nenhuma',
+    aliases: ['economia', 'eco'],
+    usage: '$economia',
+    async execute(msg, args) {
+        const userPermission = msg.member.hasPermission("MANAGE_GUILD") || msg.member.hasPermission("ADMINISTRATOR")
+
+        if (!userPermission) return msg.reply('Você não pode habilitar/desabilitar os comandos de economia')
+        const guildEconomyPermission = await guilds.indexGuild(msg.guild.id)
+
+        switch (guildEconomyPermission.economy) {
+            case true:
+                guilds.updateDataGuild({ guildId: msg.guild.id }, { economy: false })
+                return msg.reply('Foi desabilitado todos os comandos de economia!!')
+            case false:
+                guilds.updateDataGuild({ guildId: msg.guild.id }, { economy: true })
+                return msg.reply('Foi habilitado todos os comandos de economia!!')
+            default:
+                return msg.reply('Não foi possível fazer a alteração nos comandos de economia!!')
+        }
+    }
+}
