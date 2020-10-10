079b2cb6744a4d756406d22a0c6881fb020b4132
Pedro Henrique Santos Braga <gamesantos7@gmail.com>
2020-10-10 11:25:37 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -161,7 +161,7 @@ bot.on('message', async msg => {
 
     // Verifica se o canal é o canal de comando da Lilly
     if (commandChannel) {
-       if ( !commandChannel && !commandChannelPermission ) { 
+       if ( (!commandChannel) && (!commandChannelPermission) ) { 
            return msg.reply(`**Você só pode digitar comandos no canal <#${guild.commandChannel}>!!**`)
        }
    }
