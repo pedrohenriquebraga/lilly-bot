da9ba6825154390a40152e5ee327313bda9c3874
pedrohenriquebraga <gamesantos7@gmail.com>
2020-10-19 16:54:10 -0300

    Update urls

--- src/commands/utils/help.js
+++ src/commands/utils/help.js
@@ -19,8 +19,8 @@ module.exports = {
                 { name: '📨 Me convide para seu server', value: `[Ficarei muito feliz em fazer parte de seu servidor!](https://discord.com/api/oauth2/authorize?client_id=754548334328283137&permissions=8&scope=bot)` },
                 { name: '👷‍♂️ Entre em contato com o suporte', value: `[Minha equipe de suporte vai ficar feliz em receber suas dúvidas em meu servidor!](https://discord.gg/SceHNfZ)` },
                 { name: '🗳 Vote na Lilly', value: `[Vote em mim para que eu consiga mais amiguinhos!!](https://zuraaa.com/bots/754548334328283137)` },
-                { name: '🌐 Acesse meu Website', value: '[Acesse meu site para ter mais informações sobre mim](https://lilly-discordbot.herokuapp.com/)' },
-                { name: '<> Meus comandos', value: `[Veja a minha lista completa de comandos](https://lilly-discordbot.herokuapp.com/commands)` }
+                { name: '🌐 Acesse meu Website', value: '[Acesse meu site para ter mais informações sobre mim](https://lilly-website.herokuapp.com/)' },
+                { name: '<> Meus comandos', value: `[Veja a minha lista completa de comandos](https://lilly-website.herokuapp.com/commands)` }
             )
             .setFooter('Com muito amor ❤ | Lilly')
         msg.reply('', serverInfoEmbed)
