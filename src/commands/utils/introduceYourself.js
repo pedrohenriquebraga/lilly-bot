c91e36636d339e7c2e8a0bdd8b821bc44be8d8ea
pedrohenriquebraga <gamesantos7@gmail.com>
2020-11-03 11:36:39 -0300

    Add more replies to command ''

--- src/commands/utils/introduceYourself.js
+++ src/commands/utils/introduceYourself.js
@@ -8,6 +8,14 @@ module.exports = {
     usage: '$lilly',
     aliases: ['whoisyou', 'quemevoce'],
     execute(msg, args) {
-        msg.channel.send(`Olá ${msg.author}, eu sou a Lilly, um simples bot criado com o objetivo de ajudar a todos!!`)
+        const responses = [
+            `Olá ${msg.author}, eu sou a Lilly, um simples bot criado com o objetivo de ajudar a todos!!`,
+            `Eae ${msg.author}, como posso te ajudar?`,
+            `Me chamou ${msg.author}? Como posso te ajudar?`,
+            `Como vai ${msg.author}? Meu nome é Lilly e estou aqui para te ajudar e divertir!!`,
+            `Olá ${msg.author}, eu sou a Lilly!!`
+        ];
+
+        msg.channel.send(responses[Math.floor(Math.random() * responses.length)])
     }
 }
