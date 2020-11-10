2c92103bf1102798c91fb7d854812c96bd08ff10
pedrohenriquebraga <gamesantos7@gmail.com>
2020-11-10 20:31:46 -0300

    Start the shop

---
+++ src/commands/infos/shop.js
@@ -0,0 +1,36 @@
+const shop = require('../../../shop.json')
+
+module.exports = {
+  name: "shop",
+  description: "Lojinha da Lilly",
+  args: false,
+  guildOnly: true,
+  economy: true,
+  premium: false,
+  userPermissions: "Nenhuma",
+  lillyPermissions: "Nenhuma",
+  aliases: ["loja", "comprar"],
+  usage: "$shop (?id da compra)",
+  async execute(msg, args, bot) {
+    const halitaEmoji = bot.emojis.cache.find(emoji => emoji.name === "lilly_halita") || ''
+    const dindinsEmoji = bot.emojis.cache.find(emoji => emoji.name === "lilly_dindins") || ''
+    const shopEmbed = {
+      color: "#ff0092",
+      title: "🏪 | Lojinha da Lilly",
+      description: "Compre aqui os melhores produtos que só a Lilly tem (ainda não é possível realizar as compras)!!",
+      thumbnail: {
+        url: bot.user.avatarURL(),
+      },
+      fields: [],
+    };
+
+    for (item of shop.items) {
+        shopEmbed.fields.push({
+            name: '**🆔 | `' + item.id + '`  ' + `${item.name}**`,
+            value: `*${item.description}*\n**${dindinsEmoji} ${item.cost.dindins} Dindins e ${halitaEmoji} ${item.cost.halitas} Halitas**`
+        })
+    }
+
+    msg.reply('', {embed: shopEmbed})
+  },
+};
