8b682de0b5cf88c5b0be9c065f8700349480fb6c
pedrohenriquebraga <gamesantos7@gmail.com>
2020-11-18 09:22:51 -0300

    Add monitoring

---
+++ src/commands/utils/systemInfo.js
@@ -0,0 +1,39 @@
+const os = require('os')
+const osUtils = require('os-utils')
+
+module.exports = {
+    name: 'systeminfo',
+    description: 'Mostra informações do sistema da Lilly',
+    args: false,
+    guildOnly: true,
+    economy: false,
+    premium: false,
+    userPermissions: 'Nenhuma',
+    lillyPermissions: 'Nenhuma',
+    aliases: ['stinfo'],
+    usage: '$systeminfo',
+    async execute(msg, args) {
+        return osUtils.cpuFree(per => {
+            return osUtils.harddrive((total, free, used) => {
+                const mem = `${osUtils.freemem().toFixed(2)} MB/${(osUtils.totalmem()).toFixed(2)} MB`
+                let systemInfoEmbed = {
+                    fields: [
+                        { 
+                            name: "<:memoria:747166095835660469> Memória RAM Livre",
+                            value: `\`${mem}\``
+                        },
+                        { 
+                            name: "CPU  Usada",
+                            value: `\`${per.toFixed(2)} %\``
+                        },
+                        {
+                            name: "<:ssd:747165573653201066> Memória Usada",
+                            value: `\`${(used / 1024).toFixed(2)} GB/${(total / 1024).toFixed(2)} GB\``
+                        }
+                    ]
+                }
+                return msg.reply('', { embed: systemInfoEmbed })
+            })
+        })
+    }
+}
