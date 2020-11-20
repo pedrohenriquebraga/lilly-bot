eee18b8f1111c437e336fe90a244c2476bc3b57c
Pedro Henrique Santos Braga <gamesantos7@gmail.com>
2020-11-20 15:27:48 -0300

    Update daily.js

--- src/commands/economy/daily.js
+++ src/commands/economy/daily.js
@@ -18,7 +18,7 @@ module.exports = {
     const currentDate = generateDate(true).split("-");
     const member = await members.indexMember(msg.author.id);
     const valueDaily = Math.floor(
-      5000 - parseInt(currentDate[1].replace(":", ""))
+      5000 - (parseInt(currentDate[1].replace(":", "")) * 2)
     );
 
     if (!member.lastDaily) member["lastDaily"] = "";
