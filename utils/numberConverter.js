92ea1ec8e1ab87d20cd11e6f144643053e6f0546
pedrohenriquebraga <gamesantos7@gmail.com>
2020-11-24 11:09:44 -0300

    Allowed float numbers with operators

--- utils/numberConverter.js
+++ utils/numberConverter.js
@@ -2,31 +2,33 @@ module.exports = {
   converterNumber(number) {
     if (!number) return false;
     const splitNumber = number.toLowerCase().split("");
+    console.log(splitNumber);
     let separateNumber = "";
     let letterOperator = "";
     let converterNumber = 0;
     const operators = ["k", "kk", "m"];
 
     splitNumber.map((num) =>
-      !isNaN(Math.floor(num))
+      !isNaN(Number(num)) || num == "."
         ? (separateNumber += num)
         : (letterOperator += num)
     );
 
     if (!letterOperator) {
-      if (!isNaN(Math.floor(separateNumber))) return Math.floor(separateNumber);
+      if (!isNaN(Number(separateNumber)))
+        return Number.parseInt(separateNumber);
       return false;
     }
 
     switch (operators.indexOf(letterOperator)) {
       case 0:
-        converterNumber = Math.floor(separateNumber) * 1000;
+        converterNumber = Number(separateNumber) * 1000;
         break;
       case 1:
-        converterNumber = Math.floor(separateNumber) * 1000000;
+        converterNumber = Number(separateNumber) * 1000000;
         break;
       case 2:
-        converterNumber = Math.floor(separateNumber) * 1000000;
+        converterNumber = Number(separateNumber) * 1000000;
         break;
       default:
         converterNumber = NaN;
