a1f8da2f111fdfa19694eb295f78430cdcf6a9de
pedrohenriquebraga <gamesantos7@gmail.com>
2020-12-04 12:28:43 -0300

    Reduce index.js

--- index.js
+++ index.js
@@ -10,9 +10,9 @@ const lilly = require("./lilly.json");
 const { startLilly } = require("./lilly/startLilly");
 const { statusUpdate, workMachines, lottery } = require("./utils/intervals");
 const placeholdersBuilder = require("./utils/placeholderMessageBuilder")
+const verifyMessage = require("./lilly/verifymessage")
 const Discord = require("discord.js");
 const bot = new Discord.Client();
-const votosZuraaa = require("./src/votosZuraaa");
 
 const guilds = require("./src/controllers/guildsController");
 const members = require("./src/controllers/membersController");
@@ -45,100 +45,11 @@ bot.once("ready", async () => {
 });
 
 bot.on("message", async (msg) => {
-  let vote = false;
-  // Sistema de recompensas por votos
-  await votosZuraaa.verificaVotos(msg, async (user) => {
-    vote = true;
-    await user.send(lilly.defaultReply.voteReply);
+  /* Esta função é responsável por gerenciar todas ás mensagens enviadas.
+  Aqui ele verifica se é um comando válido, enviado por uma pessoa
+  válida, em uma guild válida, etc.*/
 
-    const id = String(user.id);
-    const member = await members.indexMember(id);
-    if (!member) member = await members.saveMember(id)
-
-    const money = parseInt(member.money) + 1000;
-    await members.updateDataMembers({ memberId: id }, { money: money });
-  });
-
-  // Caso a mensagem seja na verdade um voto retorna a função
-  if (vote) return;
-  if (msg.channel.type == "dm") return;
-
-  // Procura o servidor no banco de dados e o usuário que digitou o comando
-  let guild = await guilds.indexGuild(msg.guild.id);
-  if (!guild) guild = await guilds.createNewGuild(msg.guild.id);
-
-  let member = await members.indexMember(msg.author.id);
-  if (!member) member = await members.saveMember(msg.author.id)
-
-  const prefix = guild.guildPrefix || "$";
-  const commandChannel = guild.commandChannel || "";
-
-  // Verifica se a Lilly foi mencionada e retorna o prefixo do servidor
-  if (msg.content.trim() == "<@754548334328283137>") {
-    if (msg.deletable) msg.delete();
-    return msg.reply(`Meu prefixo neste servidor é \`${prefix}\`, se quiser saber a lista completa de comandos basta digitar \`${prefix}help\`!!`)
-  }
-
-  // Verifica se é um comando a mensagem
-  if (!msg.content.startsWith(prefix) || msg.author.bot) return false;
-  const args = msg.content.slice(prefix.length).trim().split(/ +/);
-  const commandName = args.shift().toLowerCase();
-  const command =
-    bot.commands.get(commandName) ||
-    bot.commands.find(
-      (cmd) => cmd.aliases && cmd.aliases.includes(commandName)
-    );
-
-  if (member.lillyBan && guild.globalMembersBan) {
-    msg.reply(lilly.defaultReply.lillyBanReply);
-    return msg.deletable ? msg.delete() : false;
-  }
-
-  // Verifica se o comando existe
-  if (!command) {
-    msg.reply(`O comando \`${prefix}\`\`${commandName}\` não existe!!`);
-
-    return msg.deletable ? msg.delete() : false;
-  }
-
-  // Verifica se comando precisa de argumentos e se esses argumentos foram passados
-  if (command.args && !args.length) {
-    const lillyPedia = require("./utils/lillyPedia");
-    msg.reply("", { embed: lillyPedia(command, msg) });
-    return msg.deletable ? msg.delete() : false;
-  }
-
-  // Verifica se o comandos é de economia e se o servidor permite o uso desse tipo de comando
-  if (!guild.economy && command.economy) {
-    msg.reply("Este servidor não permite comandos de economia!!");
-    return msg.deletable ? msg.delete() : false;
-  }
-
-  const commandChannelPermission =
-    msg.member.hasPermission("MANAGE_GUILD") ||
-    msg.member.hasPermission("ADMINISTRATOR");
-
-  // Verifica se o canal é o canal de comando da Lilly
-  if (commandChannel) {
-    if (commandChannel !== msg.channel.id && !commandChannelPermission) {
-      msg.reply(
-        `**Você só pode digitar comandos no canal <#${guild.commandChannel}>!!**`
-      );
-      return msg.deletable ? msg.delete() : false;
-    }
-  }
-
-  // Tenta executar o comando, caso de erro, retorna o erro no chat
-  try {
-    lilly.dailyCommands++;
-    command.execute(msg, args, bot);
-  } catch (error) {
-    console.error(error);
-    msg.reply(lilly.defaultReply.errorCommandReply + error + "`");
-  }
-
-  // Deleta a mensagem caso seja possível
-  if (msg.deletable) msg.delete();
+  return verifyMessage(msg, guilds, members, bot)
 });
 
 bot.on("guildMemberAdd", async (member) => {
---
+++ lilly/verifymessage.js
@@ -0,0 +1,139 @@
+const lilly = require("../lilly.json");
+
+async function verifyVote(msg) {
+    const votosZuraaa = require("../src/votosZuraaa");
+    let vote = false
+    await votosZuraaa.verificaVotos(msg, async (user) => {
+        await user.send(lilly.defaultReply.voteReply);
+    
+        const id = String(user.id);
+        const member = await members.indexMember(id);
+        if (!member) member = await members.saveMember(id)
+    
+        const money = parseInt(member.money) + 1000;
+        await members.updateDataMembers({ memberId: id }, { money: money });
+
+        return vote = true;
+    });
+
+    return vote
+}
+
+function verifyMentionBot(msg) {
+    if (msg.content.trim() == "<@754548334328283137>") {
+        return true
+    } else {
+        return false
+    }
+}
+
+function verifyLillyBan(member) {
+    if (member.lillyBan && guild.globalMembersBan) {
+        return true
+    } else return false
+}
+
+function verifyArgs(command, args) {
+    if (command.args && !args.length ) return true
+    else return false
+}
+
+function verifyCommandChannels(msg, commandChannel, commandChannelPermission) {
+    if (commandChannel !== msg.channel.id && !commandChannelPermission) {
+        return true
+    } else {
+        return false
+    }
+}
+
+
+
+// Área principal 
+
+
+
+async function verifyMessage(msg, guilds, members, bot) {
+    let vote = await verifyVote(msg);
+  
+  // Caso a mensagem seja na verdade um voto retorna a função
+  if (vote) return;
+  if (msg.channel.type == "dm") return;
+
+  // Procura o servidor no banco de dados e o usuário que digitou o comando
+  let guild = await guilds.indexGuild(msg.guild.id);
+  if (!guild) guild = await guilds.createNewGuild(msg.guild.id);
+
+  let member = await members.indexMember(msg.author.id);
+  if (!member) member = await members.saveMember(msg.author.id)
+
+  const prefix = guild.guildPrefix || "$";
+  const commandChannel = guild.commandChannel || "";
+
+  // Verifica se a Lilly foi mencionada e retorna o prefixo do servidor
+  if (verifyMentionBot(msg)) {
+    if (msg.deletable) msg.delete();
+    return msg.reply(`Meu prefixo neste servidor é \`${prefix}\`, se quiser saber a lista completa de comandos basta digitar \`${prefix}help\`!!`)
+  }
+
+  // Verifica se é um comando a mensagem
+  if (!msg.content.startsWith(prefix) || msg.author.bot) return false;
+  const args = msg.content.slice(prefix.length).trim().split(/ +/);
+  const commandName = args.shift().toLowerCase();
+  const command =
+    bot.commands.get(commandName) ||
+    bot.commands.find(
+      (cmd) => cmd.aliases && cmd.aliases.includes(commandName)
+    );
+
+  if (verifyLillyBan(member)) {
+    msg.reply(lilly.defaultReply.lillyBanReply);
+    return msg.deletable ? msg.delete() : false;
+  }
+
+  // Verifica se o comando existe
+  if (!command) {
+    msg.reply(`O comando \`${prefix}\`\`${commandName}\` não existe!!`);
+    return msg.deletable ? msg.delete() : false;
+  }
+
+  // Verifica se comando precisa de argumentos e se esses argumentos foram passados
+  if (verifyArgs(command, args)) {
+    const lillyPedia = require("../utils/lillyPedia");
+    msg.reply("", { embed: lillyPedia(command, msg) });
+    return msg.deletable ? msg.delete() : false;
+  }
+
+  // Verifica se o comandos é de economia e se o servidor permite o uso desse tipo de comando
+  if (!guild.economy && command.economy) {
+    msg.reply("Este servidor não permite comandos de economia!!");
+    return msg.deletable ? msg.delete() : false;
+  }
+
+  const commandChannelPermission =
+    msg.member.hasPermission("MANAGE_GUILD") ||
+    msg.member.hasPermission("ADMINISTRATOR");
+
+  // Verifica se o canal é o canal de comando da Lilly
+  if (commandChannel) {
+    if (verifyCommandChannels(msg, commandChannel, commandChannelPermission)) {
+      msg.reply(
+        `**Você só pode digitar comandos no canal <#${guild.commandChannel}>!!**`
+      );
+      return msg.deletable ? msg.delete() : false;
+    }
+  }
+
+  // Tenta executar o comando, caso de erro, retorna o erro no chat
+  try {
+    lilly.dailyCommands++;
+    command.execute(msg, args, bot);
+  } catch (error) {
+    console.error(error);
+    msg.reply(lilly.defaultReply.errorCommandReply + error + "`");
+  }
+
+  // Deleta a mensagem caso seja possível
+  if (msg.deletable) msg.delete();
+}
+ 
+module.exports = verifyMessage
